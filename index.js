0e81ce1c38b86747d51770a1bcf8225f6f62c129
Reni Somoye <rensomoye@gmail.com>
2023-01-27 15:46:04 -0800

    commit tree 2f04ff661247db9f5e8b20f49a08beed89e259e2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.getElementById('main');
+element.remove()
+
+// let h1 = document.querySelector('h1')
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = "Reni Somoye is the champion"
